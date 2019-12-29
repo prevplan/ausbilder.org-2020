03fe8f27a419b688bc79aec360fa8a4587d57b5c
Holger Schmermbeck <h.schmermbeck@ausbilder.org>
2019-12-29 16:05:01 +0100

    added logout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -130,6 +130,14 @@
                 </div>
             </li>
             <li class="nav-item">
+                <a class="nav-link" href="{{ route('logout') }}"
+                   onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                    <i class="fas fa-sign-out-alt"></i></a>
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                    @csrf
+                </form>
+            </li>
+            <li class="nav-item">
                 <a class="nav-link" data-widget="control-sidebar" data-slide="true" href="#"><i
                         class="fas fa-th-large"></i></a>
             </li>
