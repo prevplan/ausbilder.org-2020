620de6db56c4e8b8c823fcf76ace6bb376643958
Holger Schmermbeck <h.schmermbeck@ausbilder.org>
2019-12-29 19:08:11 +0100

    removed unnecessary ;

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,4 +14,4 @@ const mix = require('laravel-mix');
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
     .copy('resources/fonts', 'public/fonts')
-    .copy('resources/img', 'public/img');;
+    .copy('resources/img', 'public/img');
