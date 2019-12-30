16e20b9d82ea1ca2dd908e8811ea5d1789d45103
Holger Schmermbeck <h.schmermbeck@ausbilder.org>
2019-12-30 15:47:34 +0100

    added legal & privacy + copyright notice

--- app/Http/Controllers/LegalController.php
+++ app/Http/Controllers/LegalController.php
@@ -18,28 +18,17 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+namespace App\Http\Controllers;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+class LegalController extends Controller
+{
+    public function imprint()
+    {
+        return view('legal.imprint');
+    }
 
-Auth::routes(['verify' => true]);
-
-Route::get('/home', 'HomeController@index')->name('home');
-
-Route::get('admin', function () {
-    return view('admin_template');
-});
-
-Route::get('impressum', 'LegalController@imprint')->name('imprint');
-Route::get('datenschutz', 'LegalController@privacy')->name('privacy');
+    public function privacy()
+    {
+        return view('legal.privacy');
+    }
+}
