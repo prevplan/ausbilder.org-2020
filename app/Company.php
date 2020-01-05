b4a4f8591006a23cd0ce7f92f040cec4ad64cce2
Holger Schmermbeck <hosch@mailbox.org>
2020-01-05 20:51:04 +0100

    Apply fixes from StyleCI (#42)

--- app/Company.php
+++ app/Company.php
@@ -10,6 +10,6 @@ class Company extends Model
 
     public function users()
     {
-        return $this->belongsToMany(User::class)->withTimestamps();;
+        return $this->belongsToMany(User::class)->withTimestamps();
     }
 }
