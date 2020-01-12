19d8ab650bccab38166cbff14c4b35f6692f5904
abasjo <hosch@mailbox.org>
2020-01-12 14:32:00 +0000

    Apply fixes from StyleCI

--- database/seeds/PermissionsTableSeeder.php
+++ database/seeds/PermissionsTableSeeder.php
@@ -16,13 +16,13 @@ class PermissionsTableSeeder extends Seeder
         Permission::create([
             'name' => 'company.edit',
             'display_name' => 'edit company',
-            'description' => 'can edit the company'
+            'description' => 'can edit the company',
         ]);
 
         Permission::create([
             'name' => 'permissions.edit',
             'display_name' => 'edit permissions',
-            'description' => 'can edit permissions'
+            'description' => 'can edit permissions',
         ]);
 
         // attach all permissions to user one
