45b8617abbb26c93557baddfebcdb59039d156cc
Holger Schmermbeck <h.schmermbeck@ausbilder.org>
2020-01-21 22:36:20 +0100

    Update language-dropdown.blade.php

--- resources/views/layouts/language-dropdown.blade.php
+++ resources/views/layouts/language-dropdown.blade.php
@@ -23,6 +23,7 @@
     <div class="dropdown-menu dropdown-menu-md dropdown-menu-right">
         @foreach(LaravelLocalization::getSupportedLocales() as $localeCode => $properties)
             <a
+                    rel="alternate" hreflang="{{ $localeCode }}"
                     href="{{ LaravelLocalization::getLocalizedURL($localeCode, null, [], true) }}"
                     class="dropdown-item dropdown-footer">
                 {{ $properties['native'] }}
