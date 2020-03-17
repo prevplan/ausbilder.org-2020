4be3455ba2811a530b4e1e8bc94bac48c5a5ead3
abasjo <hosch@mailbox.org>
2020-03-17 06:47:59 +0000

    Apply fixes from StyleCI

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -68,5 +68,4 @@ class Kernel extends HttpKernel
         'localeCookieRedirect' => \Mcamara\LaravelLocalization\Middleware\LocaleCookieRedirect::class,
         'localeViewPath' => \Mcamara\LaravelLocalization\Middleware\LaravelLocalizationViewPath::class,
     ];
-
 }
