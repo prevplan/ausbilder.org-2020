45302c15a024a50079e0ab2632166864e601b160
abasjo <abasjo@users.noreply.github.com>
2020-09-16 10:24:55 +0000

    Apply fixes from StyleCI

--- app/Http/Controllers/SmsController.php
+++ app/Http/Controllers/SmsController.php
@@ -6,26 +6,27 @@ use Twilio\Rest\Client;
 
 class SmsController extends Controller
 {
-    public static function send($receiver, $text) {
+    public static function send($receiver, $text)
+    {
         // check number format
         if ($receiver[0] == 0 && is_numeric($receiver[0]) && $receiver[1]) { // leading zero
-            $receiver = substr_replace( $receiver, '+49', 0, 1 );
+            $receiver = substr_replace($receiver, '+49', 0, 1);
         } elseif ($receiver[0] == 0 && is_numeric($receiver[0]) && $receiver[1] == 0 && is_numeric($receiver[1])) { // two leading zero
-            $receiver = substr_replace( $receiver, '+', 0, 2 );
+            $receiver = substr_replace($receiver, '+', 0, 2);
         }
 
-        $account_sid = $_ENV["TWILIO_ACCOUNT_SID"];
-        $auth_token = $_ENV["TWILIO_AUTH_TOKEN"];
+        $account_sid = $_ENV['TWILIO_ACCOUNT_SID'];
+        $auth_token = $_ENV['TWILIO_AUTH_TOKEN'];
 
-        $twilio_number = $_ENV["TWILIO_SENDER"];
+        $twilio_number = $_ENV['TWILIO_SENDER'];
 
         $client = new Client($account_sid, $auth_token);
         $client->messages->create(
             $receiver,
-            array(
+            [
                 'from' => $twilio_number,
-                'body' => $text
-            )
+                'body' => $text,
+            ]
         );
     }
 }
